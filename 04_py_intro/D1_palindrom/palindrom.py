8a24d8ee3ce5fd60bb3ae400e6bc51beed015fa7
Benedikt Theuretzbachner <benedikt.theuretzbachner@gmail.com>
2025-01-19 16:23:48 +0100

    implement palindrom_product

--- 04_py_intro/D1_palindrom/palindrom.py
+++ 04_py_intro/D1_palindrom/palindrom.py
@@ -29,6 +29,29 @@ def is_palindrom_sentence(s: str) -> bool:
             reversed += ch
     return is_palindrom(reversed)
 
+def palindrom_product(x: int) -> int:
+    """
+        :param x: value the palindrom product must be smaller than
+        :return: the largest palindrom product smaller than x
+
+        >>> palindrom_product(1000000)
+        906609
+        >>> palindrom_product(100000)
+        99999
+    """
+
+    lp = -1
+    for i in range(999, 99, -1):
+        for j in range(i, 99, -1):
+            n: int = i*j
+            if n >= x:
+                continue
+            if n < lp:
+                break
+            if is_palindrom(str(n)):
+                lp = n
+
+    return lp
 
 if __name__ == "__main__":
     import doctest
