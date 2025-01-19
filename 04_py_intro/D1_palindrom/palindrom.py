8c900343ad6cb4ed7c17ddccc19122ed6c155c94
Benedikt Theuretzbachner <benedikt.theuretzbachner@gmail.com>
2025-01-19 19:32:40 +0100

    implement to_base

--- 04_py_intro/D1_palindrom/palindrom.py
+++ 04_py_intro/D1_palindrom/palindrom.py
@@ -22,7 +22,6 @@ def is_palindrom_sentence(s: str) -> bool:
         >>> is_palindrom_sentence("Should return false.")
         False
     """
-    
     reversed = ""
     for ch in s.lower():
         if ch.isalnum():
@@ -39,7 +38,6 @@ def palindrom_product(x: int) -> int:
         >>> palindrom_product(100000)
         99999
     """
-
     lp = -1
     for i in range(999, 99, -1):
         for j in range(i, 99, -1):
@@ -52,6 +50,25 @@ def palindrom_product(x: int) -> int:
                 lp = n
 
     return lp
+
+def to_base(number: int, base: int) -> str:
+    """
+        :param number: Zahl im 10er-System
+        :param base: Zielsystem (maximal 36)
+        :return: Zahl im Zielsystem als String
+        >>> to_base(1234,16)
+        '4D2'
+        >>> to_base(255,2)
+        '11111111'
+    """
+    out: str = ""
+    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    while number > 0:
+        digit: int = number % base
+        out += chars[digit]
+        number = int((number - digit) / base)
+
+    return out[::-1]
 
 if __name__ == "__main__":
     import doctest
