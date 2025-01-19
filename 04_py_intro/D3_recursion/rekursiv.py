d03fb0bc3285931ee3c9dcaa346786f1e81dfad8
Benedikt Theuretzbachner <benedikt.theuretzbachner@gmail.com>
2025-01-19 20:54:15 +0100

    add list comprehensions in rekursiv.py

--- 04_py_intro/D3_recursion/rekursiv.py
+++ 04_py_intro/D3_recursion/rekursiv.py
@@ -1,5 +1,7 @@
 __author__ = 'Benedikt Theuretzbachner'
 
+from time import time
+
 def M(n: int) -> int:
     """
         :param n: input for McCarthy-91 function
@@ -17,3 +19,11 @@ def M(n: int) -> int:
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+    t0 = time()
+    m_list = [M(n) for n in range(0, 200)]
+    print(m_list)
+    m_dict = {n: M(n) for n in range(0, 200)}
+    print(m_dict)
+
+    print("Took " + str((time() - t0)) + "s")
