3ec8fb1d995c45593a5934a8564d86531adfbe4b
Benedikt Theuretzbachner <benedikt.theuretzbachner@gmail.com>
2025-01-19 15:48:55 +0100

    implement is_palindrom_sentence

--- 04_py_intro/D1_palindrom/palindrom.py
+++ 04_py_intro/D1_palindrom/palindrom.py
@@ -2,7 +2,7 @@ __author__ = 'Benedikt Theuretzbachner'
 
 def is_palindrom(s: str) -> bool:
     """
-        :param number: input string
+        :param s: input string
         :return: true if s is a palindrom
 
         >>> is_palindrom("lagerregal")
@@ -10,7 +10,25 @@ def is_palindrom(s: str) -> bool:
         >>> is_palindrom("test")
         False
     """
-    return s == str(s[::-1])
+    return s == s[::-1]
+
+def is_palindrom_sentence(s: str) -> bool:
+    """
+        :param s: input string
+        :return: true if s is a palindrom sentence
+
+        >>> is_palindrom_sentence("Was it a car or a cat I saw?")
+        True
+        >>> is_palindrom_sentence("Should return false.")
+        False
+    """
+    
+    reversed = ""
+    for ch in s.lower():
+        if ch.isalnum():
+            reversed += ch
+    return is_palindrom(reversed)
+
 
 if __name__ == "__main__":
     import doctest
