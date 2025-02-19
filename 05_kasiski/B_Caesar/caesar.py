322117bef980dc5eb266295048f225a989f663a4
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-02-19 10:34:29 +0100

    implement to_lower_case in caesar.py

--- 05_kasiski/B_Caesar/caesar.py
+++ 05_kasiski/B_Caesar/caesar.py
@@ -1,11 +1,15 @@
-def to_lowercase_letter_only(plaintext: str) -> str:
-    """Wandelt den plaintext in Kleinbuchstaben um und entfernt alle Zeichen, die keine
-    Kleinbuchstaben aus dem Bereich [a..z] sind.
-    >>> caesar = Caesar()
-    >>> caesar.to_lowercase_letter_only("Wandelt den plaintext in Kleinbuchstaben um und
-    entfernt alle Zeichen, die keine Kleinbuchstaben aus dem Bereich [a..z] sind.")
-    'wandeltdenplaintextinkleinbuchstabenumundentferntallezeichendiekeinekleinbuchstaben
-    ausdembereichazsind'
-    """
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    return plaintext.lower()
+class Caesar:
+    def __init__(self, key: str) -> None:
+        self.key = key
+
+    def to_lowercase_letter_only(self, plaintext: str) -> str:
+        """Wandelt den plaintext in Kleinbuchstaben um und entfernt alle Zeichen, die keine
+        Kleinbuchstaben aus dem Bereich [a..z] sind.
+        >>> caesar = Caesar()
+        >>> caesar.to_lowercase_letter_only("Wandelt den plaintext in Kleinbuchstaben um und
+        entfernt alle Zeichen, die keine Kleinbuchstaben aus dem Bereich [a..z] sind.")
+        'wandeltdenplaintextinkleinbuchstabenumundentferntallezeichendiekeinekleinbuchstaben
+        ausdembereichazsind'
+        """
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        return str(filter(lambda ch: ch in letters, plaintext.lower()))
