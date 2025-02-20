ed6af7701a2cefc63fab3c985ab69cc7501b782a
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-02-20 12:22:38 +0100

    implement encrypt() in caesar.py

--- 05_kasiski/B_Caesar/caesar.py
+++ 05_kasiski/B_Caesar/caesar.py
@@ -1,15 +1,48 @@
 class Caesar:
-    def __init__(self, key: str) -> None:
-        self.key = key
+    def __init__(self, key: str | None = None) -> None:
+        if key:
+            if not key.isalpha() or not len(key) == 1:
+                raise ValueError("key must be a single alphanumeric letter")
+            self.key = key
+        else:
+            self.key = "a"
 
     def to_lowercase_letter_only(self, plaintext: str) -> str:
         """Wandelt den plaintext in Kleinbuchstaben um und entfernt alle Zeichen, die keine
         Kleinbuchstaben aus dem Bereich [a..z] sind.
         >>> caesar = Caesar()
-        >>> caesar.to_lowercase_letter_only("Wandelt den plaintext in Kleinbuchstaben um und
-        entfernt alle Zeichen, die keine Kleinbuchstaben aus dem Bereich [a..z] sind.")
-        'wandeltdenplaintextinkleinbuchstabenumundentferntallezeichendiekeinekleinbuchstaben
-        ausdembereichazsind'
+        >>> caesar.to_lowercase_letter_only("Wandelt den plaintext in Kleinbuchstaben um und entfernt alle Zeichen, die keine Kleinbuchstaben aus dem Bereich [a..z] sind.")
+        'wandeltdenplaintextinkleinbuchstabenumundentferntallezeichendiekeinekleinbuchstabenausdembereichazsind'
         """
         letters = "abcdefghijklmnopqrstuvwxyz"
-        return str(filter(lambda ch: ch in letters, plaintext.lower()))
+        return "".join(filter(lambda ch: ch.isalpha(), plaintext.lower()))
+
+    def encrypt(self, plaintext: str, key: str | None = None) -> str:
+        """key ist ein Buchstabe, der definiert, um wieviele Zeichen verschoben wird.
+        Falls kein key übergeben wird, nimmt übernimmt encrypt den Wert vom Property.
+        >>> caesar=Caesar("b")
+        >>> caesar.key
+        'b'
+        >>> caesar.encrypt("hallo")
+        'ibmmp'
+        >>> caesar.decrypt("ibmmp")
+        'hallo'
+        >>> caesar.encrypt("hallo", "c")
+        'jcnnq'
+        >>> caesar.encrypt("xyz", "c")
+        'zab'
+        """
+        if key is None:
+            key = self.key
+
+        out = ""
+        for ch in plaintext:
+            out += chr(((ord(ch) - 97) + (ord(key) - 97)) % 26 + 97)
+
+        return out
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
