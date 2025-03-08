a59e199481364b73711f335ab89627c60c85e6bd
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-03-08 14:35:39 +0100

    implement find_files_by_ext.py

--- 06_py_adminscripting/find_files_by_ext.py
+++ 06_py_adminscripting/find_files_by_ext.py
@@ -3,4 +3,14 @@ from pathlib import Path
 __author__ = "Benedikt theuretzbachner"
 
 
-p = Path(input("Enter filepath > "))
+path = Path(input("[filepath]> "))
+if not path.is_dir():
+    raise Exception("Path must be an existing directory")
+
+suffix = input("[file-extension]> ")
+if not suffix.startswith("."):
+    suffix = "." + suffix
+
+
+for file_path in path.rglob(f"*{suffix}"):
+    print(file_path)
