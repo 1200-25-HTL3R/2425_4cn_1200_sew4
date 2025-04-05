2d619e6de399409dc505d56b94bdd81cd6513d6e
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-04-05 14:33:18 +0200

    implement edit1

--- 08_comprehension/spellcheck.py
+++ 08_comprehension/spellcheck.py
@@ -7,5 +7,22 @@ def split_word(wort: str) -> list[tuple[str, str]]:
     return [(wort[:i], wort[i:]) for i in range(0, len(wort) + 1)]
 
 
+def edit1(wort: str) -> set[str]:
+    typos = set()
+    for s_word in split_word(wort):
+        l = s_word[0]
+        r = s_word[1]
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+        if len(r) >= 2:
+            typos.add(l + r[1:])
+            typos.add(l + r[1] + r[0] + r[2:])
+        if len(r) >= 1:
+            typos.update(set(l + ch + r[1:] for ch in alphabet))
+        typos.update(set(l + ch + r for ch in alphabet))
+
+    return typos
+
+
 if __name__ == "__main__":
-    print(split_word("abc"))
+    print(edit1("abc"))
