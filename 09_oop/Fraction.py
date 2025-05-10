59e5b1b51a227d87d93d45e596bdfb784aada74c
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-05-10 17:47:45 +0200

    implement __add__

--- 09_oop/Fraction.py
+++ 09_oop/Fraction.py
@@ -1,32 +1,34 @@
+import math
+
+
 class Fraction:
     _numerator = 0
     _denominator = 1
     _neg = False
 
-    def __init__(self, zaehler=0, nenner=1) -> None:
-        self._numerator = abs(zaehler)
+    def __init__(self, numerator=0, denominator=1) -> None:
+        if denominator == 0:
+            raise ArithmeticError("Denominator can't be 0")
 
-        if nenner == 0:
-            raise ArithmeticError("denominator can't be 0")
-        self._denominator = abs(nenner)
-
-        if zaehler * nenner < 0:
-            self._neg = True
+        gcd = math.gcd(numerator, denominator)
+        self._numerator = int(numerator / gcd)
+        self._denominator = int(denominator / gcd)
 
     def __str__(self):
-        sign = ""
-        if self._neg:
-            sign = "-"
+        if self.numerator == 0:
+            return "0"
 
-        num = self.numerator / self.denominator
-        num = int(num)
-        if num == 0:
-            return sign + str(self.numerator) + "/" + str(self.denominator)
+        prefix = self.numerator / self.denominator
+        prefix = int(prefix)
+        if prefix == 0:
+            return str(self.numerator) + "/" + str(self.denominator)
 
-        self.numerator -= num * self.denominator
-        num_str = str(num) + " "
+        new_numerator = self.numerator - prefix * self.denominator
+        prefix = abs(prefix)
+        if new_numerator == 0:
+            return str(prefix)
 
-        return sign + num_str + str(self.numerator) + "/" + str(self.denominator)
+        return str(prefix) + " " + str(new_numerator) + "/" + str(self.denominator)
 
     def __repr__(self) -> str:
         return f"Fraction({self.numerator}, {self.denominator})"
@@ -38,6 +40,7 @@ class Fraction:
     @numerator.setter
     def numerator(self, numerator):
         self._numerator = numerator
+        self = Fraction(numerator, self.denominator)
 
     @property
     def denominator(self):
@@ -46,8 +49,22 @@ class Fraction:
     @denominator.setter
     def denominator(self, denominator):
         self._denominator = denominator
+        self = Fraction(self.numerator, denominator)
+
+    def __add__(self, other):
+        if not isinstance(other, Fraction):
+            return self.__add__(Fraction(other))
+
+        numerator = (
+            self.numerator * other.denominator + other.numerator * self.denominator
+        )
+        denominator = self.denominator * other.denominator
+
+        return Fraction(numerator, denominator)
 
 
 if __name__ == "__main__":
-    f = Fraction(1, -3)
-    print(f)
+    f = Fraction(1, -4)
+    f2 = Fraction(1, 4)
+
+    print(f + f2)
