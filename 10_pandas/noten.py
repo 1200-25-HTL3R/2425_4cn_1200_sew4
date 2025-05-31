c3ce66dd34236e202f12e6444fbc33fd945e13ae
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-05-31 15:37:01 +0200

    Implement read_xml

--- 10_pandas/noten.py
+++ 10_pandas/noten.py
@@ -1,6 +1,8 @@
 #!/bin/python
 
 import argparse
+import re
+import pandas as pd
 
 __author__ = "Benedikt Theuretzbachner"
 
@@ -26,7 +28,35 @@ def add_args() -> None:
     output.add_argument("-q", "--quiet", action="store_true", help="keine Textausgabe")
 
 
+def read_xml(filename: str) -> pd.DataFrame:
+    with open(filename) as f:
+        content = f.read()
+
+    result = []
+
+    student_pattern = re.compile(r"<Schueler>.*?</Schueler>", flags=re.DOTALL)
+    students = re.findall(student_pattern, content)
+
+    student_data_pattern = re.compile(
+        r"<(Nummer|Anrede|Vorname|Nachname|Geburtsdatum)>(.*)</\1>"
+    )
+    for s in students:
+        student_data = map(lambda t: t[1], re.findall(student_data_pattern, s))
+        result.append(student_data)
+
+    print(result)
+    df = pd.DataFrame(
+        result,
+        columns=["Nummer", "Anrede", "Vorname", "Nachname", "Geburtsdatum"],
+        dtype=str,
+    )
+
+    return df
+
+
 if __name__ == "__main__":
     add_args()
 
     parser.parse_args()
+
+    print(read_xml("schueler.xml"))
