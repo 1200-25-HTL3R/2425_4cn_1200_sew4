fdecc798d38ffbd6df1b20777c18f9b72d82bbe4
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-05-31 17:26:54 +0200

    Implement filtering of subjects

--- 10_pandas/noten.py
+++ 10_pandas/noten.py
@@ -1,14 +1,20 @@
 #!/bin/python
 
 import argparse
+from os.path import join
 import re
 import pandas as pd
 
 __author__ = "Benedikt Theuretzbachner"
 
-parser = argparse.ArgumentParser(
+parser: argparse.ArgumentParser = argparse.ArgumentParser(
     description="noten.py by Benedikt Theuretzbachner / HTL Rennweg"
 )
+args = None
+
+df_grades = pd.DataFrame()
+df_students = pd.DataFrame()
+df_joined = pd.DataFrame()
 
 
 def add_args() -> None:
@@ -53,15 +59,29 @@ def read_xml(filename: str) -> pd.DataFrame:
     return df
 
 
+def filter_df(filter: set):
+    for col in df_grades.columns:
+        if col not in filter:
+            df_grades.drop(columns=[col], inplace=True)
+
+
 if __name__ == "__main__":
     add_args()
 
     args = parser.parse_args()
+    join_col = "Nummer"
+    if args.m:
+        join_col = args.m
 
     df_grades = pd.read_csv(args.n, sep=";").astype(str)
-    df_grades.set_index("Nummer", inplace=True)
-    df_students = read_xml(args.s).astype(str)
-    df_students.set_index("Nummer", inplace=True)
+    df_grades.set_index(join_col, inplace=True)
+    if args.f:
+        filter: set = set(str(args.f).split(","))
+        filter_df(filter)
 
-    df_joined = df_grades.join(df_students, how="outer", on="Nummer")
+    df_students = read_xml(args.s).astype(str)
+    df_students.set_index(join_col, inplace=True)
+
+    df_joined = df_students.join(df_grades)
+
     print(df_joined)
